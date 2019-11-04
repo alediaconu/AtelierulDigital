912c4e290eb92aa02654617f0f368ec9372f884d
Alexandra Diaconu <alexandra.diaconu96@gmail.com>
2019-11-04 17:29:08 -0200

    lab2

---
+++ src/lab2/Challenge1/Main.java
@@ -0,0 +1,15 @@
+package lab2;
+
+public class Main {
+    public static void main(String[] args) { //psvm->genereaza automat
+
+        Fighter f1 = new Fighter("First ", 100, 20);
+        Fighter f2 = new Fighter("First ", 100, 20 );
+        BoxingMatch boxingMatch = new BoxingMatch(f1, f2);
+        boxingMatch.fight();
+        System.out.println("Result= "+ boxingMatch.fight());
+        System.out.println(f2);
+        System.out.println(f1);
+        System.out.println("NumberOfFighters= " + Fighter.getNumberOfFighters());
+    }
+}
